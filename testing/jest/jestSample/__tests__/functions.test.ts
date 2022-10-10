165336be4d6d5bbf98983638ae3ea1bb4ad0668a
“cashev” <42924021+cashev@users.noreply.github.com>
2022-10-10 20:17:05 +0900

    途中までのテストを一時コミット

--- testing/jest/jestSample/__tests__/functions.test.ts
+++ testing/jest/jestSample/__tests__/functions.test.ts
@@ -1,1 +1,22 @@
 // todo: ここに単体テストを書いてみましょう！
+import {asyncSumOfArray, sumOfArray} from "../functions";
+
+test('SumOfArray', () => {
+    expect(sumOfArray([1, 1])).toBe(2);
+});
+
+test('SumOfArray', () => {
+    expect(sumOfArray([1])).toBe(1);
+});
+
+test('SumOfArray', () => {
+    expect(sumOfArray([1, 2, 3])).toBe(6);
+});
+
+test('SumOfArray', () => {
+    expect(() => sumOfArray([])).toThrow();
+});
+
+test('asyncSumOfArray', async () => {
+    await expect(asyncSumOfArray([1, 2])).resolves.toBe(3);
+});
