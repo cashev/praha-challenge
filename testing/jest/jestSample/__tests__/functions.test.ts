47756426ea74838ca64706e80dabe1b796e37bdc
“cashev” <42924021+cashev@users.noreply.github.com>
2022-10-12 23:12:52 +0900

    describeを追加してtestを関数ごとに記載

--- testing/jest/jestSample/__tests__/functions.test.ts
+++ testing/jest/jestSample/__tests__/functions.test.ts
@@ -1,22 +1,29 @@
-// todo: ここに単体テストを書いてみましょう！
 import {asyncSumOfArray, sumOfArray} from "../functions";
 
-test('SumOfArray', () => {
-    expect(sumOfArray([1, 1])).toBe(2);
+describe("SumOfArray関数", () => {
+    test("要素が1つの配列を渡すと、その値が返る", () => {
+        expect(sumOfArray([1])).toBe(1);
+    });
+    
+    test("要素が複数の配列を渡すと、その合計が返る", () => {
+        expect(sumOfArray([1, 2, 3])).toBe(6);
+    });
+   
+    test("空の配列を渡すと、例外となる", () => {
+        expect(() => sumOfArray([])).toThrow();
+    });
 });
 
-test('SumOfArray', () => {
-    expect(sumOfArray([1])).toBe(1);
-});
-
-test('SumOfArray', () => {
-    expect(sumOfArray([1, 2, 3])).toBe(6);
-});
-
-test('SumOfArray', () => {
-    expect(() => sumOfArray([])).toThrow();
-});
-
-test('asyncSumOfArray', async () => {
-    await expect(asyncSumOfArray([1, 2])).resolves.toBe(3);
+describe("asyncSumOfArray関数", () => {
+    test("要素が1つの配列を渡すと、その値が返る", async () => {
+        await expect(asyncSumOfArray([1])).resolves.toBe(1);
+    });
+    
+    test("要素が複数の配列を渡すと、その合計が返る", async () => {
+        await expect(sumOfArray([1, 2, 3])).toBe(6);
+    });
+    
+    test("空の配列を渡すと、例外となる", async () => {
+        await expect(() => sumOfArray([])).toThrow();
+    });
 });
