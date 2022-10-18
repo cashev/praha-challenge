4cadcbaa86ddaa02246d8241e65334091d542080
“cashev” <42924021+cashev@users.noreply.github.com>
2022-10-18 21:51:59 +0900

    nameApiServiceのテストケースを追加
    axiosが例外を投げたパターンのテストを追加
    それぞれのテストケースについてコメントを記載

--- testing/jest/jestSample/__tests__/nameApiService.test.ts
+++ testing/jest/jestSample/__tests__/nameApiService.test.ts
@@ -5,18 +5,23 @@ import { NameApiService } from "../nameApiService";
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-describe("", () => {
-  test("", async () => {
-    const firstName = { first_name: "Rob" };
+describe("getFirstName関数", () => {
+  test("取得したfirstNameがMAX_LENGTHより短い場合、そのfirstNameが返る", async () => {
+    const firstName = { first_name: "Burt" };
     const resp = { data: firstName };
     mockedAxios.get.mockResolvedValue(resp);
-    await expect(new NameApiService().getFirstName()).resolves.toBe("Rob");
+    await expect(new NameApiService().getFirstName()).resolves.toBe("Burt");
   });
 
-  test("", async () => {
-    const firstName = { first_name: "Anjelica" };
+  test("取得したfirstNameがMAX_LENGTHより長い場合、例外を投げる", async () => {
+    const firstName = { first_name: "Erwin" };
     const resp = { data: firstName };
     mockedAxios.get.mockResolvedValue(resp);
+    await expect(new NameApiService().getFirstName()).rejects.toThrow("firstName is too long!");
+  });
+
+  test("firstNameの取得に失敗した場合、例外を投げる", async () => {
+    mockedAxios.get.mockImplementation(() => {throw Error()});
     await expect(new NameApiService().getFirstName()).rejects.toThrow();
   });
 });
