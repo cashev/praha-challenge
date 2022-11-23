47cbd09622d407dc9fa48559d5b1d5e489d84f22
cashev <42924021+cashev@users.noreply.github.com>
2022-11-23 04:53:56 +0000

    ビジュアルリグレッションテストを追加

--- testing/my-app/src/storybook.test.js
+++ testing/my-app/src/storybook.test.js
@@ -1,2 +1,23 @@
 import initStoryshots from '@storybook/addon-storyshots';
-initStoryshots();
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+import path from 'path'
+import playwrite from 'playwright';
+
+let Browser;
+const getCustomBrowser = async () => {
+  Browser = await playwrite.chromium.launch();
+  return Browser;
+};
+
+initStoryshots({ 
+    suite: 'Image storyshots',
+    test: imageSnapshot({
+        storybookUrl: `file://${path.resolve(__dirname, '../storybook-static')}`,
+        getCustomBrowser,
+        setupTimeout: 30000,
+    }),
+});
+
+test.afterAll = async () => {
+    await Browser.close();
+};
