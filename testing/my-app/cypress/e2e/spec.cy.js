225add5bbf4fd0918b2cffea63230790e04fc1f9
cashev <42924021+cashev@users.noreply.github.com>
2022-11-29 14:37:30 +0000

    cypress を導入
    npm run cy:run をする前に以下でvirtual monitorを設定する
    https://repo1.dso.mil/platform-one/big-bang/apps/library-charts/gluon/-/issues/7

---
+++ testing/my-app/cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('spec.cy.is', () => {
+    it('should visit', () => {
+        cy.visit('/')
+    })
+})
+
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+        expect(true).to.equal(true)
+    })
+})
