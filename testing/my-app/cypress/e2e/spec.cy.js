14d8b9101052158f077b961c81888407a6441084
cashev <42924021+cashev@users.noreply.github.com>
2022-12-04 02:25:37 +0000

    引き分けなどのCypressテストを追加

--- testing/my-app/cypress/e2e/spec.cy.js
+++ testing/my-app/cypress/e2e/spec.cy.js
@@ -1,13 +1,64 @@
+describe('First Player is X', () => {
+    it('should show Next Player is X', () => {
+        cy.visit('/');
+        cy.get('[data-e2e=status]').should('have.text', '次のプレイヤー: X');
+    })
+})
+
+describe('Turn to Next Player is O', () => {
+    it('should show Next Player is O', () => {
+        cy.visit('/');
+        cy.get('[data-e2e=square-index-4]').click();
+        cy.get('[data-e2e=status]').should('have.text', '次のプレイヤー: O');
+    })
+})
+
+describe('Turn to Next Player is X', () => {
+    it('should show Next Player is X', () => {
+        cy.visit('/');
+        cy.get('[data-e2e=square-index-4]').click();
+        cy.get('[data-e2e=square-index-0]').click();
+        cy.get('[data-e2e=status]').should('have.text', '次のプレイヤー: X');
+    })
+})
+
 describe('Cross Player wins', () => {
     it('should show Winner: X', () => {
         cy.visit('/');
-
         cy.get('[data-e2e=square-index-4]').click();
         cy.get('[data-e2e=square-index-3]').click();
         cy.get('[data-e2e=square-index-0]').click();
         cy.get('[data-e2e=square-index-6]').click();
         cy.get('[data-e2e=square-index-8]').click();
-
         cy.get('[data-e2e=status]').should('have.text', 'Winner: X');
+    })
+})
+
+describe('Circle Player wins', () => {
+    it('should show Winner: X', () => {
+        cy.visit('/');
+        cy.get('[data-e2e=square-index-4]').click();
+        cy.get('[data-e2e=square-index-0]').click();
+        cy.get('[data-e2e=square-index-8]').click();
+        cy.get('[data-e2e=square-index-1]').click();
+        cy.get('[data-e2e=square-index-7]').click();
+        cy.get('[data-e2e=square-index-2]').click();
+        cy.get('[data-e2e=status]').should('have.text', 'Winner: O');
+    })
+})
+
+describe('Draw', () => {
+    it('should show Draw!', () => {
+        cy.visit('/');
+        cy.get('[data-e2e=square-index-4]').click();
+        cy.get('[data-e2e=square-index-0]').click();
+        cy.get('[data-e2e=square-index-1]').click();
+        cy.get('[data-e2e=square-index-7]').click();
+        cy.get('[data-e2e=square-index-2]').click();
+        cy.get('[data-e2e=square-index-6]').click();
+        cy.get('[data-e2e=square-index-3]').click();
+        cy.get('[data-e2e=square-index-5]').click();
+        cy.get('[data-e2e=square-index-8]').click();
+        cy.get('[data-e2e=status]').should('have.text', 'Draw!');
     })
 })
