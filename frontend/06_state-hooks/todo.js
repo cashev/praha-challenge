2f7db959f17da5865e085d9cabae14df20ab03f6
cashev <42924021+cashev@users.noreply.github.com>
2023-09-03 13:27:24 +0000

    change to function component by chatGPT

--- frontend/06_state-hooks/todo.js
+++ frontend/06_state-hooks/todo.js
@@ -1,142 +1,143 @@
-var TodoBox = React.createClass({
-	getInitialState: function () {
-		return {
-			data: [
-				{"id":"00001","task":"Wake up","complete":"false"},
-				{"id":"00002","task":"Eat breakfast","complete":"false"},
-        {"id":"00003","task":"Go to work","complete":"false"}
-			]
-		};
-	},
-	generateId: function () {
-		return Math.floor(Math.random()*90000) + 10000;
-	},
-	handleNodeRemoval: function (nodeId) {
-		var data = this.state.data;
-		data = data.filter(function (el) {
-			return el.id !== nodeId;
-		});
-		this.setState({data});
-		return;
-	},
-	handleSubmit: function (task) {
-		var data = this.state.data;
-		var id = this.generateId().toString();
-		var complete = 'false';
-		data = data.concat([{id, task, complete}]);
-		this.setState({data});
-	},
-	handleToggleComplete: function (nodeId) {
-		var data = this.state.data;
-		for (var i in data) {
-			if (data[i].id == nodeId) {
-				data[i].complete = data[i].complete === 'true' ? 'false' : 'true';
-				break;
-			}
-		}
-		this.setState({data});
-		return;
-	},
-	render: function() {
-		return (
-			<div className="well">
-				<h1 className="vert-offset-top-0">To do:</h1>
-				<TodoList data={this.state.data} removeNode={this.handleNodeRemoval} toggleComplete={this.handleToggleComplete} />
-				<TodoForm onTaskSubmit={this.handleSubmit} />
-			</div>
-		);
-	}
-});
+import React, { useState, useRef } from "react";
+import ReactDOM from "react-dom";
 
-var TodoList = React.createClass({
-	removeNode: function (nodeId) {
-		this.props.removeNode(nodeId);
-		return;
-	},
-	toggleComplete: function (nodeId) {
-		this.props.toggleComplete(nodeId);
-		return;
-	},
-	render: function() {
-		var listNodes = this.props.data.map(function (listItem) {
-			return (
-				<TodoItem key={listItem.id} nodeId={listItem.id} task={listItem.task} complete={listItem.complete} removeNode={this.removeNode} toggleComplete={this.toggleComplete} />
-			);
-		},this);
-		return (
-			<ul className="list-group">
-				{listNodes}
-			</ul>
-		);
-	}
-});
+const TodoBox = () => {
+  const [data, setData] = useState([
+    { id: "00001", task: "Wake up", complete: "false" },
+    { id: "00002", task: "Eat breakfast", complete: "false" },
+    { id: "00003", task: "Go to work", complete: "false" }
+  ]);
 
-var TodoItem = React.createClass({
-	removeNode: function (e) {
-		e.preventDefault();
-		this.props.removeNode(this.props.nodeId);
-		return;
-	},
-	toggleComplete: function (e) {
-		e.preventDefault();
-		this.props.toggleComplete(this.props.nodeId);
-		return;
-	},
-	updateClass: function () {
-		
-	},
-	render: function() {
-		var classes = 'list-group-item clearfix';
-		if (this.props.complete === 'true') {
-			classes = classes + ' list-group-item-success';
-		}
-		return (
-			<li className={classes}>
-				{this.props.task}
-				<div className="pull-right" role="group">
-					<button type="button" className="btn btn-xs btn-success img-circle" onClick={this.toggleComplete}>&#x2713;</button> <button type="button" className="btn btn-xs btn-danger img-circle" onClick={this.removeNode}>&#xff38;</button>
-				</div>
-			</li>
-		);
-	}
-});
+  const generateId = () => {
+    return Math.floor(Math.random() * 90000) + 10000;
+  };
 
-var TodoForm = React.createClass({
-	doSubmit: function (e) {
-		e.preventDefault();
-		var task = React.findDOMNode(this.refs.task).value.trim();
-		if (!task) {
-			return;
-		}
-		this.props.onTaskSubmit(task);
-		React.findDOMNode(this.refs.task).value = '';
-		return;
-	},
-	render: function() {
-		return (
-			<div className="commentForm vert-offset-top-2">
-				<hr />
-				<div className="clearfix">
-					<form className="todoForm form-horizontal" onSubmit={this.doSubmit}>
-						<div className="form-group">
-							<label htmlFor="task" className="col-md-2 control-label">Task</label>
-							<div className="col-md-10">
-								<input type="text" id="task" ref="task" className="form-control" placeholder="What do you need to do?" />
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-md-10 col-md-offset-2 text-right">
-								<input type="submit" value="Save Item" className="btn btn-primary" />
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		);
-	}
-});
+  const handleNodeRemoval = (nodeId) => {
+    const newData = data.filter((el) => el.id !== nodeId);
+    setData(newData);
+  };
 
-React.render(
-	<TodoBox />,
-	document.getElementById('todoBox')
-);
+  const handleSubmit = (task) => {
+    const id = generateId().toString();
+    const complete = "false";
+    const newData = [...data, { id, task, complete }];
+    setData(newData);
+  };
+
+  const handleToggleComplete = (nodeId) => {
+    const newData = data.map((item) => {
+      if (item.id === nodeId) {
+        item.complete = item.complete === "true" ? "false" : "true";
+      }
+      return item;
+    });
+    setData(newData);
+  };
+
+  return (
+    <div className="well">
+      <h1 className="vert-offset-top-0">To do:</h1>
+      <TodoList
+        data={data}
+        removeNode={handleNodeRemoval}
+        toggleComplete={handleToggleComplete}
+      />
+      <TodoForm onTaskSubmit={handleSubmit} />
+    </div>
+  );
+};
+
+const TodoList = ({ data, removeNode, toggleComplete }) => {
+  return (
+    <ul className="list-group">
+      {data.map((listItem) => (
+        <TodoItem
+          key={listItem.id}
+          nodeId={listItem.id}
+          task={listItem.task}
+          complete={listItem.complete}
+          removeNode={removeNode}
+          toggleComplete={toggleComplete}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const TodoItem = ({ nodeId, task, complete, removeNode, toggleComplete }) => {
+  const classes =
+    complete === "true"
+      ? "list-group-item clearfix list-group-item-success"
+      : "list-group-item clearfix";
+
+  return (
+    <li className={classes}>
+      {task}
+      <div className="pull-right" role="group">
+        <button
+          type="button"
+          className="btn btn-xs btn-success img-circle"
+          onClick={() => toggleComplete(nodeId)}
+        >
+          &#x2713;
+        </button>
+        <button
+          type="button"
+          className="btn btn-xs btn-danger img-circle"
+          onClick={() => removeNode(nodeId)}
+        >
+          &#xff38;
+        </button>
+      </div>
+    </li>
+  );
+};
+
+const TodoForm = ({ onTaskSubmit }) => {
+  const taskRef = useRef(null);
+
+  const doSubmit = (e) => {
+    e.preventDefault();
+    const task = taskRef.current.value.trim();
+    if (!task) {
+      return;
+    }
+    onTaskSubmit(task);
+    taskRef.current.value = "";
+  };
+
+  return (
+    <div className="commentForm vert-offset-top-2">
+      <hr />
+      <div className="clearfix">
+        <form className="todoForm form-horizontal" onSubmit={doSubmit}>
+          <div className="form-group">
+            <label htmlFor="task" className="col-md-2 control-label">
+              Task
+            </label>
+            <div className="col-md-10">
+              <input
+                type="text"
+                id="task"
+                ref={taskRef}
+                className="form-control"
+                placeholder="What do you need to do?"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-10 col-md-offset-2 text-right">
+              <input
+                type="submit"
+                value="Save Item"
+                className="btn btn-primary"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default TodoBox;
