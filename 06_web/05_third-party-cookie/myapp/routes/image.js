e5e6f3c85713d24c8cd89671bad37f2b21638ac7
cashev <42924021+cashev@users.noreply.github.com>
2023-10-27 16:35:21 +0000

    set cookie in image request

---
+++ 06_web/05_third-party-cookie/myapp/routes/image.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET image. */
+router.get('/', function(req, res, next) {
+  res.cookie('name', 'hoge-image', {maxAge: 1000 * 60, httpOnly: true});
+  request('https://picsum.photos/200/300', { encoding: null }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      // Set the Content-Type header to indicate that the response is an image
+      res.set('Content-Type', 'image/jpeg');
+
+      // Send the image binary data as the response
+      res.send(body);
+    } else {
+      res.status(404).send('Image not found');
+    }
+  });
+});
+
+module.exports = router;
