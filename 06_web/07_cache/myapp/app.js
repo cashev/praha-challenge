429e01b892503e6aecfdbfaeac298f732110caed
cashev <42924021+cashev@users.noreply.github.com>
2023-11-15 14:11:45 +0000

    課題02 add Router

--- 06_web/07_cache/myapp/app.js
+++ 06_web/07_cache/myapp/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const withCacheRouter = require('./routes/with-cache');
+const noCacheRouter = require('./routes/no-cache');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/with-cache', withCacheRouter);
+app.use('/no-cache', noCacheRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
