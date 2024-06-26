7c3ee8dce7a03140885456410cb3cb12697f4854
cashev <42924021+cashev@users.noreply.github.com>
2024-06-27 00:00:55 +0900

    コメント機能を実装

--- 10_mvp/01_api/cli/src/index.ts
+++ 10_mvp/01_api/cli/src/index.ts
@@ -10,6 +10,7 @@ const program = new Command();
 program
   .argument('<owner>', 'Github Owner Name')
   .argument('<repo>', 'Github Repository Name')
+  .option('-i --issue <issue>', 'Issue number to add comment to')
   .option('-c --comment <comment>', 'Comment to add to the issue')
   .parse(process.argv);
 
@@ -17,14 +18,16 @@ const owner = program.args[0];
 const repo = program.args[1];
 const options = program.opts();
 
-const fetchIssuesOctokit = async (owner: string, repo: string) => {
+// Fetch issues from the repository
+const fetchIssues = async (owner: string, repo: string) => {
   try {
     const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
       owner,
       repo,
     });
 
-    data.filter((issue: any) => !issue.pull_request).forEach((issue: any) => {
+    data.filter((issue: any) => !issue.pull_request).sort((a: any, b: any) => a.number - b.number)
+    .forEach((issue: any) => {
       console.log(`#${issue.number} - ${issue.title}`);
     });
   } catch (error: any) {
@@ -32,8 +35,31 @@ const fetchIssuesOctokit = async (owner: string, repo: string) => {
   }
 };
 
-if (options.comment) {
-  process.exit(1);
+// Add comment to the issue
+const addComment = async (owner: string, repo: string, issue: number, comment: string) => {
+  try {
+    const { data } = await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+      owner,
+      repo,
+      issue_number: issue,
+      body: comment,
+    });
+    console.log('Comment added:', data.html_url);
+  } catch (error: any) {
+    console.error('Error adding comment:', error.message);
+  }
+}
+
+if (options.issue || options.comment) {
+  if (!options.issue) {
+    console.error('Issue number is required');
+    process.exit(1);
+  }
+  if (!options.comment) {
+    console.error('Comment is required');
+    process.exit(1);
+  }
+  addComment(owner, repo, parseInt(options.issue), options.comment);
 } else  {
-  fetchIssuesOctokit(owner, repo);
+  fetchIssues(owner, repo);
 }
