e5e88c2de19fdd07fcff4a3f62cf83cbd9021621
cashev <42924021+cashev@users.noreply.github.com>
2024-06-26 23:40:41 +0900

    use Octokit

--- 10_mvp/01_api/cli/src/index.ts
+++ 10_mvp/01_api/cli/src/index.ts
@@ -1,38 +1,39 @@
-import axios from 'axios';
 import { Command } from 'commander';
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({ 
+  auth: process.env.GITHUB_TOKEN,
+});
 
 const program = new Command();
 
 program
-  .version('1.0.0')
-  .description('CLI to fetch GitHub issues')
-  .option('-r, --repo <repo>', 'GitHub repository in the format owner/repo')
+  .argument('<owner>', 'Github Owner Name')
+  .argument('<repo>', 'Github Repository Name')
+  .option('-c --comment <comment>', 'Comment to add to the issue')
   .parse(process.argv);
 
+const owner = program.args[0];
+const repo = program.args[1];
 const options = program.opts();
 
-if (!options.repo) {
-  console.error('Please specify a repository using the -r or --repo option.');
-  process.exit(1);
-}
-
-const fetchIssues = async (repo: string) => {
+const fetchIssuesOctokit = async (owner: string, repo: string) => {
   try {
-    const response = await axios.get(`https://api.github.com/repos/${repo}/issues`);
-    const issues = response.data;
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+      owner,
+      repo,
+    });
 
-    const filteredIssues = issues.filter((issue: any) => !issue.pull_request);
-
-    filteredIssues.forEach((issue: any) => {
+    data.filter((issue: any) => !issue.pull_request).forEach((issue: any) => {
       console.log(`#${issue.number} - ${issue.title}`);
     });
   } catch (error: any) {
-    if (axios.isAxiosError(error)) {
-      console.error('Error fetching issues:', error.message);
-    } else {
-      console.error('Unexpected error:', error);
-    }
+    console.error('Error fetching issues:', error.message);
   }
 };
 
-fetchIssues(options.repo);
+if (options.comment) {
+  process.exit(1);
+} else  {
+  fetchIssuesOctokit(owner, repo);
+}
