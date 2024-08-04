3acb2ca4cc5e209860ac4183d85c1170df83effc
cashev <42924021+cashev@users.noreply.github.com>
2024-08-05 01:27:13 +0900

    Airtableから一覧取得

--- 10_mvp/04_airtable/myapp/src/app.ts
+++ 10_mvp/04_airtable/myapp/src/app.ts
@@ -1,11 +1,34 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const airtableToken = process.env.AIRTABLE_PAT;
+const baseId = process.env.AIRTABLE_BASE_ID;
+const tableName = process.env.AIRTABLE_TABLE_NAME;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/users', async (req, res) => {
+  try {
+    const response = await axios.get(`https://api.airtable.com/v0/${baseId}/${tableName}`, {
+      headers: {
+        Authorization: `Bearer ${airtableToken}`
+      }
+    });
+  
+    res.json(response.data.records);
+  } catch (error) {
+    console.error('Error fetching data from Airtable:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
