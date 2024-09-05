a1f7fd2d874e69af9b85076bbcf50cdf5c0d862a
cashev <42924021+cashev@users.noreply.github.com>
2024-09-06 01:16:43 +0900

    リファクタリング

--- 11_extra/03_refactor/employee.ts
+++ 11_extra/03_refactor/employee.ts
@@ -6,57 +6,106 @@ export class Employee {
   public static readonly SALESMAN: number = 1;
   public static readonly MANAGER: number = 2;
   public static readonly SUPER_MANAGER: number = 3;
+  private type: EmployeeType
   public constructor(
-    private _type: number,
-    private _monthlySalary: number,
-    private _comission: number,
-    private _bonus: number
-  ) {}
+    protected _type: number,
+    private readonly _monthlySalary: number,
+    protected _comission: number,
+    protected _bonus: number
+  ) {
+    this.type = EmployeeType.newType(_type);
+  }
+
+  public get getMonthlySalary() {
+    return this._monthlySalary;
+  }
+  public get getComission() {
+    return this._comission;
+  }
+  public get getBonus() {
+    return this._bonus;
+  }
 
   // memo:給与計算
   public payAmount() {
-    switch (this._type) {
-      case Employee.ENGINEER:
-        return this._monthlySalary;
-      case Employee.SALESMAN:
-        return this._monthlySalary + this._comission;
-      case Employee.MANAGER:
-        return this._monthlySalary + this._bonus;
-      case Employee.SUPER_MANAGER:
-        return this._monthlySalary + this._comission + this._bonus;
-      default:
-        throw new Error("invalid employee type");
-    }
+    return this.type.payAmount(this);
   }
   // memo: 退職プランを適用するか否か
   public hasRetirementPlan() {
-    switch (this._type) {
+    return this.type.hasRetirementPlan();
+  }
+  // memo: 取締役か否か
+  public isBoardMember() {
+    return this.type.isBoardMember();
+  }
+}
+
+abstract class EmployeeType {
+  abstract payAmount(employee: Employee): number;
+  abstract hasRetirementPlan(): boolean;
+  abstract isBoardMember(): boolean;
+
+  public static newType(typeCode: number): EmployeeType {
+    switch (typeCode) {
       case Employee.ENGINEER:
-        return true;
+        return new Engineer();
       case Employee.SALESMAN:
-        return false;
+        return new Salesman();
       case Employee.MANAGER:
-        return true;
+        return new Manager();
       case Employee.SUPER_MANAGER:
-        return true;
+        return new SuperManager();
       default:
         throw new Error("invalid employee type");
     }
   }
-  // memo: 取締役か否か
+}
+
+class Engineer extends EmployeeType {
+  public payAmount(employee: Employee): number {
+    return employee.getMonthlySalary;
+  }
+  public hasRetirementPlan() {
+    return true;
+  }
   public isBoardMember() {
-    switch (this._type) {
-      case Employee.ENGINEER:
-        return false;
-      case Employee.SALESMAN:
-        return false;
-      case Employee.MANAGER:
-        return true;
-      case Employee.SUPER_MANAGER:
-        return true;
-      default:
-        throw new Error("invalid employee type");
-    }
+    return false;
+  }
+}
+
+class Salesman extends EmployeeType {
+  public payAmount(employee: Employee): number {
+    return employee.getMonthlySalary + employee.getComission;
+  }
+  public hasRetirementPlan() {
+    return false;
+  }
+  public isBoardMember() {
+    return false;
+  }
+}
+
+class Manager extends EmployeeType {
+  public payAmount(employee: Employee): number {
+    return employee.getMonthlySalary + employee.getBonus;
+  }
+  public hasRetirementPlan() {
+    return true;
+  }
+  public isBoardMember() {
+    return true;
+  }
+}
+
+class SuperManager extends EmployeeType {
+  public payAmount(employee: Employee): number {
+    return employee.getMonthlySalary + employee.getComission + employee.getBonus;
+  }
+  public hasRetirementPlan() {
+    return true;
+  }
+  public isBoardMember() {
+    return true;
   }
 }
 
