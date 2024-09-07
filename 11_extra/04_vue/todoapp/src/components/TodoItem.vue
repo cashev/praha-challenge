ad9159cf94d07fddac3083c192064b144601b511
cashev <42924021+cashev@users.noreply.github.com>
2024-09-07 16:59:39 +0900

    toggle complete

--- 11_extra/04_vue/todoapp/src/components/TodoItem.vue
+++ 11_extra/04_vue/todoapp/src/components/TodoItem.vue
@@ -1,24 +1,31 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 import type { Todo } from './TodoBox.vue'
 
-defineProps<{
+const props = defineProps<{
   todo: Todo
 }>()
 
-const complete = () => {
-  console.log('complete')
+const isComplete = ref(props.todo.complete)
+
+const toggleComplete = () => {
+  isComplete.value = !isComplete.value
 }
 
 const remove = () => {
   console.log('remove')
 }
+
+const classes = computed(() => {
+  return isComplete.value ? 'list-group-item list-group-item-success' : 'list-group-item'
+})
 </script>
 
 <template>
-  <li class="list-group-item clearfix">
+  <li :class="classes">
     {{ todo.task }}
     <div class="pull-right" role="group">
-      <button type="button" @click="complete" class="btn btn-xs btn-success img-circle">
+      <button type="button" @click="toggleComplete" class="btn btn-xs btn-success img-circle">
         &#x2713;
       </button>
       <button type="button" @click="remove" class="btn btn-xs btn-danger img-circle">
